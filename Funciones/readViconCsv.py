fc00d11e4c97e378a1163ac87985fc1df1f31da0
joselvira <46655528+joselvira@users.noreply.github.com>
2021-01-10 10:08:05 +0100

    Add files via upload

--- Funciones/readViconCsv.py
+++ Funciones/readViconCsv.py
@@ -10,13 +10,58 @@ from __future__ import division, print_function
 
 import numpy as np
 import pandas as pd
-import scipy.signal
+import xarray as xr
+#import scipy.signal
 
 __author__ = 'Jose Luis Lopez Elvira'
-__version__ = 'v.1.0'
-__date__ = '17/06/2019'
+__version__ = 'v.2.0.0'
+__date__ = '13/12/2020'
 
-def read_vicon_csv(nombreArchivo, nomBloque='Model Outputs', returnFrec=False, separador=','):
+"""
+Modificaciones:
+13/12/2020, v2.0.0
+            - con el argumento formatoxArray se puede pedir que devuelva los datos en formato xArray
+"""
+
+def read_vicon_csv(nombreArchivo, nomBloque='Model Outputs', separador=',', returnFrec=False, formatoxArray=False):
+    """    
+    Parameters
+    ----------
+    nombreArchivo : string
+        ruta del archivo a abrir.
+        
+    nomBloque : string
+        tipo de datos a leer en el archivo original.
+        'Model Outputs', 'Trajectories' o 'Devices'
+    
+    separador : string
+        caracter separador de los datos
+    
+    returnFrec : bool
+        si es True devuelve un int con la frecuencia de muestreo
+    
+    formatoxArray : bool
+        si es true devuelve los datos en formato xArray    
+    
+    
+    Returns
+    -------
+    data : datos leidos en formato DataFrame de Pandas o DataArray de xArray.
+    frec: frecuencia de registro de los datos.
+    
+    
+    
+    Examples
+    --------
+    >>> dfDatos = read_vicon_csv(nombreArchivo, nomBloque='Model Outputs')
+    >>> dfDatos, frecuencia = read_vicon_csv(nombreArchivo, nomBloque='Trajectories', returnFrec=True)
+    
+    >>> #Con formato dataarray de xArray
+    >>> daDatos = read_vicon_csv(nombreArchivo, nomBloque='Trajectories', formatoxArray=True)
+
+    """   
+    daReturn=xr.DataArray()
+    
     with open(nombreArchivo, mode='rt') as f:
         numLinea=0
         #busca etiqueta del inicio del bloque
@@ -51,28 +96,83 @@ def read_vicon_csv(nombreArchivo, nomBloque='Model Outputs', returnFrec=False, s
           
     finBloque = numLinea-1 #quita 1 para descontar la línea vacía
     
+    
+    
     #primero asigna los nombres según el propio archivo
     nomVars=['Frame', 'Sub Frame']
     for i in range(2,len(nomCols),3):
-        nomVars.append(nomColsVar[i].split(':')[1]+'_'+nomCols[i])#X
-        nomVars.append(nomColsVar[i].split(':')[1]+'_'+nomCols[i+1])#Y
-        nomVars.append(nomColsVar[i].split(':')[1]+'_'+nomCols[i+2])#Z
-            
-    #carga los datos
+        if "'" not in nomCols[i] and "''" not in nomCols[i]:            
+            nomVars.append(nomColsVar[i].split(':')[1]+'_'+nomCols[i])#X
+            nomVars.append(nomColsVar[i].split(':')[1]+'_'+nomCols[i+1])#Y
+            nomVars.append(nomColsVar[i].split(':')[1]+'_'+nomCols[i+2])#Z
+    
+    # [i for i in nomColsVar if "'" in i]
+    # nomColsVar = [i for i in nomColsVar if "'" not in i]
+        
+    
+    
+    
+    #carga todos los datos
     provisional= np.genfromtxt(nombreArchivo, skip_header= inicioBloque+5, max_rows=finBloque-inicioBloque-1, delimiter=separador)
     #provisional=provisional[:, :len(nomVars)] #recorta solo hasta las variables 
     
-    if returnFrec:
-        return pd.DataFrame(provisional, columns=nomVars), frecuencia
-    else:
-        return pd.DataFrame(provisional, columns=nomVars)
+    #Convierte los datos en pandas dataframe. Pasa solo los que no son de velocidad o aceleración
+    dfReturn = pd.DataFrame(provisional[:, :len(nomVars)], columns=nomVars)
+    
+    # #Elimina las columnas de velocidad y aceleración, si las hay
+    # borrarColsVA = dfReturn.filter(regex='|'.join(["'", "''"])).columns
+    # dfReturn = dfReturn.drop(columns=borrarColsVA)
+    
+    #Si hace falta lo pasa a xArray
+    if formatoxArray:
+        #transforma los datos en xarray
+        x=dfReturn.filter(regex='|'.join(['_x','_X'])).to_numpy().T
+        y=dfReturn.filter(regex='|'.join(['_y','_Y'])).to_numpy().T
+        z=dfReturn.filter(regex='|'.join(['_z','_Z'])).to_numpy().T
+        data=np.stack([x,y,z])
+        
+        #Quita el identificador de la coordenada del final
+        canales = [i.split(':')[-1] for i in nomColsVar[2:len(nomCols):3]] #se pone hasta len(nomCols) porque a veces queda una línea al final suelta que descuadra
+                        
+        n_frames = x.shape[1]
+        channels = canales
+        time = np.arange(start=0, stop=n_frames / frecuencia, step=1 / frecuencia)
+        coords = {}
+        coords["axis"] = ['x', 'y', 'z']
+        coords["channel"] = channels
+        coords["time"] = time
+        
+        daReturn=xr.DataArray(
+                    data=data,
+                    dims=('axis', 'channel', 'time'),
+                    coords=coords,
+                    name=nomBloque,
+                    #**kwargs,
+                )
+            
+    
+    if formatoxArray and returnFrec:
+        return dfReturn, daReturn, frecuencia
+    elif formatoxArray:
+        return dfReturn, daReturn
+    elif returnFrec:
+        return dfReturn, frecuencia
+    else:   
+        return dfReturn
 
 # =============================================================================
 # %%        
 # =============================================================================
 if __name__ == '__main__':
-    nombreArchivo = r"G:\Mi unidad\Investigacion\Proyectos\BikeFitting\Fatiga\Datos Cinematica Congreso Estudiantes\JUNTOS\01_Carrillo_FIN.csv"
+    from pathlib import Path
+    ruta_Archivo = r'G:\Mi unidad\Investigacion\Proyectos\BikeFitting\Fatiga\Datos Cinematica Congreso Estudiantes\JUNTOS\01_Carrillo_FIN.csv'
+    nombreArchivo = Path(ruta_Archivo)
     
     dfDatos = read_vicon_csv(nombreArchivo, nomBloque='Model Outputs')
-    dfDatos, frecuencia = read_vicon_csv(nombreArchivo, nomBloque='Model Outputs', returnFrec=True)
+    dfDatos, frecuencia = read_vicon_csv(nombreArchivo, nomBloque='Trajectories', returnFrec=True)
     
+    #Con formato dataarray de xArray
+    dfDatos, daDatos = read_vicon_csv(nombreArchivo, nomBloque='Trajectories', formatoxArray=True)
+    
+    dfDatos['Right_Toe_Z'].plot()
+    daDatos.sel(channel='Right_Toe', axis='z').plot.line()
